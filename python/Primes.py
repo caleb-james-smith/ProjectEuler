8b4d60ed51710ef26b7c6e03ce5386215c67db7e
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-07-05 11:10:43 -0500

    Sieve of Eratosthenes: speed up prime number search by a factor of about 200.

--- python/Primes.py
+++ python/Primes.py
@@ -32,9 +32,9 @@ class Primes:
     
     def isPrime(self, number, values=[]):
         # note: 1 is not prime
-        # speed up by checking some divisors
         if number == 1:
             return False
+        # speed up by checking some divisors from list of primes
         if values:
             if number in values:
                 return True
@@ -45,7 +45,7 @@ class Primes:
         # check for divisors up to square root
         return not self.divsToSqrt(number)
 
-    def calcPrimes(self, max_val):
+    def calcPrimesBasic(self, max_val):
         if max_val > 1000:
             # speed up by using first set of primes
             first_max = 1000
@@ -55,6 +55,26 @@ class Primes:
         else:
             primes = [x for x in range(1, max_val) if self.isPrime(x)]
         self.setPrimes(primes)
+
+    def calcPrimesAdvanced(self, max_val):
+        if max_val < 1:
+            print("ERROR: max_val needs to be a positive integer")
+            return
+        elif max_val == 1:
+            self.setPrimes([])
+            return
+        is_prime_list = max_val * [True]
+        is_prime_list[0] = False 
+        is_prime_list[1] = False 
+        for i in range(2, int(np.sqrt(max_val)) + 1):
+            if is_prime_list[i]:
+                j = 2
+                while i * j < max_val:
+                    is_prime_list[i * j] = False
+                    j += 1
+        primes = [i for i in range(len(is_prime_list)) if is_prime_list[i]] 
+        self.setPrimes(primes)
+        return
 
     def setPrimes(self, primes):
         self.primes = primes
--- python/test_primes.py
+++ python/test_primes.py
@@ -1,15 +1,35 @@
 # test_primes.py
 
 from Primes import Primes
+import time
 
 def test():
     P = Primes()
     limits = [10 ** i for i in range(7)]
 
+    t0 = time.time()
+    
     for lim in limits:
-        P.calcPrimes(lim)
+        P.calcPrimesBasic(lim)
         primes = P.getPrimes()
         print("limit: {0:.1e}, number of primes: {1}".format(lim, len(primes)))
+        #print(primes)
+
+    t1 = time.time()
+    
+    run_time = t1 - t0
+    print("run time: {0:.3f} seconds".format(run_time))
+    
+    for lim in limits:
+        P.calcPrimesAdvanced(lim)
+        primes = P.getPrimes()
+        print("limit: {0:.1e}, number of primes: {1}".format(lim, len(primes)))
+        #print(primes)
+    
+    t2 = time.time()
+    
+    run_time = t2 - t1
+    print("run time: {0:.3f} seconds".format(run_time))
     
 test()
 

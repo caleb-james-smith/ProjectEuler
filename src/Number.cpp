bb43698ffc27013611ee9becb63ff7390283d245
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-09-04 16:15:39 -0500

    Implement addition of two numbers that are strings

--- src/Number.cpp
+++ src/Number.cpp
@@ -17,18 +17,65 @@ void Number::setValue(std::string value)
 Number Number::add(Number number)
 {
     Number result = Number();
+    
     std::string value1 = this->getValue();
     std::string value2 = number.getValue();
+    std::string primary;
+    std::string secondary;
+    std::string answer;
+
+    if (value1.length() >= value2.length())
+    {
+        printf("Use value 1 (%s) as primary\n", value1.c_str());
+        primary   = value1;
+        secondary = value2;
+    }
+    else
+    {
+        printf("Use value 2 (%s) as primary\n", value2.c_str());
+        primary   = value2;
+        secondary = value1;
+    }
+
+    // carry to next digit 
+    int q = 0;
+    for (int i = 0; i < primary.length(); ++i)
+    {
+        // get int value for each character
+        int x = primary[primary.length() - 1 - i] - '0';
+        int y = 0;
+        if (i < secondary.length())
+        {
+            y = secondary[secondary.length() - 1 - i] - '0';
+        }
+        //std::cout << primary[primary.length() - 1 - i] << ": " << x << std::endl;
+        //std::cout << secondary[secondary.length() - 1 - i] << ": " << y << std::endl;
+        
+        //printf("input: x = %d, y = %d, q = %d ---> ", x, y, q);
+        int z = x + y + q;
+        if (z > 9)
+        {
+            z = z - 10;
+            q = 1;
+        }
+        else
+        {
+            q = 0;
+        }
+        //printf("output: z = %d, q = %d\n", z, q);
+        answer.append(std::to_string(z));
+    }
+    if (q > 0)
+    {
+        printf("q = %d\n", q);
+        answer.append(std::to_string(q));
+    }
+    // reverse string
+    reverse(answer.begin(), answer.end());
+    printf("answer: %s\n", answer.c_str());
     
-    const char* a1 = &value1[0];
-    const char* b1 = &value2[0];
-    int a = atoi(a1);
-    int b = atoi(b1);
     
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    int c = a + b;
-    number.setValue(std::to_string(c));
+    number.setValue(answer);
     return number;
 }
 

d97472078a32d7d35f9572ea066521fc5d95a28c
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-07-13 11:15:56 -0500

    Add Rust hello world program.

---
+++ rust/hello_world.rs
@@ -0,0 +1,11 @@
+// This is a comment in Rust.
+
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+    println!("Bonjour!");
+}
+

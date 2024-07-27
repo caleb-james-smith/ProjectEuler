b7574c41fbb35164c89d4180a85ddd570a1590a4
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-07-27 16:54:34 -0500

    Solve Problem 4 in Rust.

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -1,7 +1,9 @@
 // Import libraries and modules
 use std::time::{Instant, Duration};
-pub mod prob_003;
+
 pub mod primes;
+pub mod prob_003;
+pub mod prob_004;
 
 // This is the main function
 fn main() {
@@ -10,7 +12,8 @@ fn main() {
     println!("---------------");
     // Calculate run time
     let start_time: Instant = Instant::now();
-    prob_003::run();
+    //prob_003::run();
+    prob_004::run();
     let end_time: Instant = Instant::now();
     let run_time: Duration = end_time.duration_since(start_time);
     println!("run time: {:.2?}", run_time);
---
+++ rust_project/src/prob_004.rs
@@ -0,0 +1,61 @@
+// prob_004.rs
+
+// Problem 4
+//
+// A palindromic number reads the same both ways.
+// The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+//
+// Find the largest palindrome made from the product of two 3-digit numbers.
+//
+
+// check if number reads the same both ways
+fn palindrome(number: i64) -> bool {
+    // get string from integer
+    let num_string: String = number.to_string();
+    // reverse string
+    let num_string_reversed: String = num_string.chars().rev().collect();
+    // check if string is equal to its reverse
+    if num_string == num_string_reversed {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Solve the problem
+fn solve(number: i64) -> i64 {
+    println!("Running solve()...");
+    
+    // Since we plan to change result, we need to make it mutable using "mut".
+    let mut result: i64 = 0;
+
+    let mut num_products: i64 = 0; 
+    let val_start: i64  = 100;
+    let val_stop: i64   = 1000;
+
+    for i in val_start..val_stop {
+        for j in i..val_stop {
+            num_products += 1;
+            let product: i64 = i * j;
+            let is_palindrome: bool = palindrome(product);
+            //println!("(i, j) = ({i}, {j}): product = {product}, is_palindrome = {is_palindrome}");
+            if is_palindrome {
+                println!("(i, j) = ({i}, {j}): product = {product}, is_palindrome = {is_palindrome}");
+                if product > result {
+                    result = product;
+                }
+            }
+        }
+    }
+
+    println!("num_products: {num_products}");
+    result
+}
+
+// Run the calculation for this problem
+pub fn run() {
+    println!("Running run()... go go go!");
+    let number: i64  = 1;
+    let answer: i64 = solve(number);
+    println!("answer: {answer}");
+}

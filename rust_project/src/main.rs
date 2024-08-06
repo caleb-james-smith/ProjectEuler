b7839218bdfd23d39484a8e2353faefdfee763fc
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-08-06 18:29:22 -0500

    Solve Problem 7 using Rust.

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -4,6 +4,7 @@ pub mod primes;
 pub mod prob_003;
 pub mod prob_004;
 pub mod prob_005;
+pub mod prob_007;
 
 // This is the main function
 fn main() {
@@ -14,7 +15,8 @@ fn main() {
     let start_time: Instant = Instant::now();
     //prob_003::run();
     //prob_004::run();
-    prob_005::run();
+    //prob_005::run();
+    prob_007::run();
     let end_time: Instant = Instant::now();
     let run_time: Duration = end_time.duration_since(start_time);
     println!("run time: {:.2?}", run_time);

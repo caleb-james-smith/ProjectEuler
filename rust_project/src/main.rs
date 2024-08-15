a4a833a3b4047996b76703ab0fc09de2ad02b4fd
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-08-15 15:20:44 -0500

    Solve Problem 10 in Rust.

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -5,6 +5,7 @@ pub mod prob_003;
 pub mod prob_004;
 pub mod prob_005;
 pub mod prob_007;
+pub mod prob_010;
 
 // This is the main function
 fn main() {
@@ -16,7 +17,8 @@ fn main() {
     //prob_003::run();
     //prob_004::run();
     //prob_005::run();
-    prob_007::run();
+    //prob_007::run();
+    prob_010::run();
     let end_time: Instant = Instant::now();
     let run_time: Duration = end_time.duration_since(start_time);
     println!("run time: {:.2?}", run_time);
--- rust_project/src/prob_010.rs
+++ rust_project/src/prob_010.rs
@@ -10,3 +10,34 @@
 // Import libraries and modules
 use crate::primes;
 
+// Solve the problem
+fn solve(number: i64) -> i64 {
+    println!("Running solve()...");
+    
+    // Since we plan to change result, we need to make it mutable using "mut".
+    let mut result: i64 = 0;
+
+    // Get prime numbers up to a max value
+    let prime_numbers: Vec<i64> = primes::get_primes_advanced(number);
+    let n_primes: i64 = prime_numbers.len() as i64;
+    
+    println!("number: {number}");
+    println!("n_primes: {n_primes}");
+    
+    // Get sum of primes
+    for p in prime_numbers {
+        result += p;
+    }
+
+    println!("Completed solve()!");
+    result
+}
+
+// Run the calculation for this problem
+pub fn run() {
+    println!("Solving Problem 10.");
+    println!("Running run()...");
+    let number: i64  = 2_000_000;
+    let answer: i64 = solve(number);
+    println!("answer: {answer}");
+}
